8c086fa890c8b3436b35d62031186c9fa161bc17
florian <berthelot_florian@yahoo.fr>
2024-10-11 16:17:04 +0200

    ok

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -7,11 +7,17 @@ terraform {
     }
 }
 
+variable "credentials_file" {
+  type = string
+}
+
 provider "google" {
     project = "terraform-etl"
+    credentials = file(var.credentials_file)
 }
 
 resource "google_project_service" "run_api" {
   project = "terraform-etl"  # Remplace par ton ID de projet
   service = "run.googleapis.com"
 }
+
